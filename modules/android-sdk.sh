6bb0ba6fcca63f56f44808eff220eda86579ce85
Mike Cunneen <mike@appworkshop.net>
2024-12-09 12:37:16 +0800

    fix(android-sdk): :bug: fix issue with android sdkmanager path

--- modules/android-sdk.sh
+++ modules/android-sdk.sh
@@ -2,8 +2,8 @@ androidSDK() {
     local THISMODULEDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
     # Android SDK Manager Cleanup
-    command -v ${ANDROID_HOME}/tools/bin/sdkmanager >/dev/null 2>&1 && {
-
+    command -v ${ANDROID_HOME}/cmdline-tools/latest/bin/sdkmanager >/dev/null 2>&1 && {
+        local SDKMANAGER_COMMAND="${ANDROID_HOME}/cmdline-tools/latest/bin/sdkmanager"
         local ANDROIDHOME_BEFORE=$(du -hs $ANDROID_HOME | cut -f1)
 
         echo "=== android sdkmanager cleanup ==="
@@ -17,13 +17,15 @@ androidSDK() {
         # create temporary output file
         local OUTFILENAME=$(mktemp)
 
-        local SDKMANAGER_OUTPUT=$(sdkmanager --list_installed --include_obsolete)
+        local SDKMANAGER_OUTPUT=$(${SDKMANAGER_COMMAND} --list_installed --include_obsolete)
+
         echo "${SDKMANAGER_OUTPUT}" |
             awk -f "${THISMODULEDIR}/android-sdk.awk" -v outputFile="${OUTFILENAME}"
 
         if [ $? -ne 0 ]; then
-            echo "    WARNING: we couldn't parse the output of 'sdkmanager --list_installed --include_obsolete'"
-            echo "    (very likely there's just nothing to remove)"
+            echo "    WARNING: we could not parse the output of 'sdkmanager --list_installed --include_obsolete'"
+            echo "    \(very likely there is just nothing to remove\)"
+            cat "${OUTFILENAME}"
             return 100
         fi
 
@@ -31,14 +33,14 @@ androidSDK() {
         cat "${OUTFILENAME}" | sed 's/^/      - /'
 
         echo "    - uninstalling packages..."
-        sdkmanager --uninstall --package_file="${OUTFILENAME}"
+        ${SDKMANAGER_COMMAND} --uninstall --package_file="${OUTFILENAME}"
         local ANDROIDHOME_AFTER=$(du -hs $ANDROID_HOME | cut -f1)
 
         echo "  - BEFORE ANDROID SDKMANAGER CLEANUP: ${ANDROIDHOME_BEFORE} (${ANDROID_HOME})"
         echo "  - AFTER ANDROID SDKMANAGER CLEANUP: ${ANDROIDHOME_AFTER}"
         addHint "- You may need to run android's SDK manager to restore the Android SDK versions you were using"
     } || {
-        echo "'${ANDROID_HOME}/tools/bin/sdkmanager' command not found"
+        echo "'${ANDROID_HOME}/cmdline-tools/latest/bin/sdkmanager' command not found"
         return 1
     }
 }
