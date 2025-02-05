7c56838eee1fa245bf81b040ac4ee4b17dbca9bb
Mike Cunneen <mike@appworkshop.net>
2025-02-05 21:54:00 +0800

    fix(library-caches): :bug: handle permissions issue

--- modules/library-caches.sh
+++ modules/library-caches.sh
@@ -1,6 +1,6 @@
 # remove everything in ~/Library/Caches
 libraryCaches() {
-    local LIBRARYCACHESIZE=$(du -hs "${HOME}/Library/Caches" | cut -f1)
+    local LIBRARYCACHESIZE=$((du -hs "${HOME}/Library/Caches" | cut -f1 ) || echo "ERROR: COULD NOT RUN 'du'")
     echo "=== emptying ~/Library/Caches...(${LIBRARYCACHESIZE}) ==="
-    rm -rf "${HOME}/Library/Caches"    
+    rm -rf "${HOME}/Library/Caches" || echo "=== ERROR! COULD NOT empty ~/Library/Caches. Continuing. ==="
 }
