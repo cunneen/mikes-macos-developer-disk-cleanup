2aace79ab5570049af7bfd87908cbfd4a5f9334f
Mike Cunneen <mike@appworkshop.net>
2025-02-05 16:52:22 +0800

    fix(homebrew-caches): :recycle: try to detect brew location

--- modules/homebrew-caches.sh
+++ modules/homebrew-caches.sh
@@ -1,11 +1,21 @@
 homebrewCaches(){
+    local BREW;
+    if [ -f /opt/homebrew/bin/brew]; then
+        # apple silicon
+        BREW=/opt/homebrew/bin/brew
+    elif [ -f /usr/local/bin/brew ]; then
+        # intel mac
+        BREW=/usr/local/bin/brew
+    else
+        BREW=/home/linuxbrew/.linuxbrew/bin/brew
+    fi
     # Homebrew Caches
-    command -v brew >/dev/null 2>&1 && {
+    command -v ${BREW} >/dev/null 2>&1 && {
         echo "=== cleaning up homebrew caches ==="
         local SIZEBEFORE=$(du -hs ${HOME}/Library/Caches/Homebrew | cut -f1)
         # set +e: don't exit on error (brew cleanup sometimes has permissions problems)
         set +e
-        brew cleanup --prune=all
+        ${BREW} cleanup --prune=all
         if [ $? -ne 0 ]; then
             echo "!!! failed to clean up homebrew caches !!!"
             echo "    check the output above for more info"
@@ -14,5 +24,8 @@ homebrewCaches(){
         set -e
         local SIZEAFTER=$(du -hs ${HOME}/Library/Caches/Homebrew | cut -f1)
         echo "    cache size before: ${SIZEBEFORE}; after: ${SIZEAFTER}"
+    } || {
+        echo "brew command not found"
+    	return 1;
     }
 }
