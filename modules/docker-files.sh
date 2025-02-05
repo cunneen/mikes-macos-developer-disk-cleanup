604512591d2763cb5be4bc425b79d968c1697de6
Mike Cunneen <mike@appworkshop.net>
2025-02-05 18:15:09 +0800

    fix(docker-files): :bug: docker daemon detection

--- modules/docker-files.sh
+++ modules/docker-files.sh
@@ -3,8 +3,8 @@ dockerFiles() {
     command -v docker >/dev/null 2>&1 && {
         echo "=== docker ==="
         local DOCKER_RUNNING=0;
-        (docker version && DOCKER_RUNNING=1) || (DOCKER_RUNNING=0;)
-        if [ $DOCKER_RUNNING -ne 1 ]; then
+        docker version && DOCKER_RUNNING=1 || DOCKER_RUNNING=0;
+        if [ ! $DOCKER_RUNNING == 1 ]; then
             echo "======= ERROR: docker daemon not running; skipping docker cleanup ======="
         else
             echo " BEFORE DOCKER CLEANUP:"
